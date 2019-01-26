50051ed27692caa85ecb3229b0750460acec559f
Peter Tucker <ptucker@whitworth.edu>
2019-01-26 14:41:30 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,18 @@
+/*
+FEEDBACK
+
+Read from the file correctly: 20/20%
+Implement class hierarchy correctly: 20/20%
+Populate appropriate data structures with shape data: 15/15%
+Show list of shapes correctly: 15/15%
+Show specific shape properties correctly: 15/15%
+Well-structured, well-commented code: 8/15%
+
+
+Comments:
+  separate the displayInfo code into a separate functions.
+  need images
+*/
 
 import java.util.Vector;
 import java.io.*;
